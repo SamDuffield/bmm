07cfc4c2f6933f17cb15b5065acf99ae776e1bf7
Sam Duffield <sddd2@cam.ac.uk>
2020-02-09 20:12:30 +0000

    Create MMParticles class

--- inference/smc.py
+++ inference/smc.py
@@ -11,8 +11,48 @@ import numpy as np
 import tools.edges
 
 
+class MMParticles:
+    """
+    Class to store trajectories from map-matching algorithm.
+
+    In particular contains the following objects:
+    self.n: int
+        number of particles
+    self.latest_observation_time: float
+        time of most recently received observation
+    self.particles: list of numpy.ndarrays, length = n_samps
+        each numpy.ndarray with shape = (_, 7)
+        columns: t, u, v, k, alpha, n_inter, d
+            t: seconds, observation time
+            u: int, edge start node
+            v: int, edge end node
+            k: int, edge key
+            alpha: in [0,1], position along edge
+            n_inter: int, number of options if intersection
+            d: metres, distance travelled since previous observation time
+    """
+    def __init__(self, initial_positions):
+        """
+        Initiate storage of trajectories with some start positions as input.
+        :param initial_positions: list-like
+            each element is list-like of length 4 with elements u, v, k, alpha
+            u: int, edge start node
+            v: int, edge end node
+            k: int, edge key
+            alpha: in [0,1], position along edge
+        """
+        self.n = len(initial_positions)
+        self.particles = [np.zeros((1, 7)) for _ in range(self.n)]
+        for i in range(self.n):
+            self.particles[i][0, 1:5] = initial_positions[i]
+
+    @property
+    def latest_observation_time(self):
+        return self.particles[0][-1, 0]
+
+
 def initiate_particles(graph, first_observation, n_samps, d_refine=1, gps_sd=7, truncation_distance=None):
-    '''
+    """
     Initiate start of a trajectory by sampling points around the first observation.
     Note that coordinate system of inputs must be the same, typically a UTM projection (not longtitude-latitude!).
     See tools/graph.py and data/preprocess.py to ensure both your grapha and polyline data are projected to UTM.
@@ -36,17 +76,8 @@ def initiate_particles(graph, first_observation, n_samps, d_refine=1, gps_sd=7, 
         metres
         distance beyond which to assume zero likelihood probability
         required only for first observation
-    :return: list of numpy.ndarrays, length = n_samps
-        each numpy.ndarray with shape = (1, 7)
-        columns: t, u, v, k, alpha, n_inter, d
-            t: seconds, observation time (= 0 here)
-            u: int, edge start node
-            v: int, edge end node
-            k: int, edge key
-            alpha: in [0,1], position along edge
-            n_inter: int, number of options if intersection (= 0 here)
-            d: metres, distance travelled since previous observation (= 0 here)
-    '''
+    :return: MMParticles object
+    """
     if truncation_distance is None:
         truncation_distance = gps_sd * 3
 
@@ -60,12 +91,7 @@ def initiate_particles(graph, first_observation, n_samps, d_refine=1, gps_sd=7, 
     # Sample indices according to weights
     sampled_indices = np.random.choice(len(weights), n_samps, replace=True, p=weights)
 
-    # Initiate array
-    sampled_points = np.zeros((n_samps, 7))
-    sampled_points[:, 1:5] = dis_points[sampled_indices, :4]
-
-    # Change to list of numpy.ndarrays
-    return [np.atleast_2d(sampled_points[i, :]) for i in range(n_samps)]
+    return MMParticles(dis_points[sampled_indices, :4])
 
 
 def update_particles(graph, particles, time_interval, new_observation, d_refine=1, d_max=None,
