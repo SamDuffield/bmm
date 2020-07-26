0076e6481675f0ea3427829154d3795aeab5d840
SamDuffield <sddd2@cam.ac.uk>
2020-07-26 18:46:57 +0100

    some sim results

--- simulated_data/simulated_em.py
+++ simulated_data/simulated_em.py
@@ -21,7 +21,7 @@ cam_graph = cambridge_graph()
 timestamps = 15
 
 gen_model = bmm.GammaMapMatchingModel()
-gen_model.max_speed = 30
+#gen_model.max_speed = 30
 gen_model.zero_dist_prob_neg_exponent = -np.log(0.1)/timestamps
 gen_model.distance_params['a_speed'] = 1.
 gen_model.distance_params['b_speed'] = 0.1
@@ -44,7 +44,7 @@ while np.any(len_obs < 10):
     for i in range(num_routes):
         if len_obs[i] < 10:
             routes[i] = sample_route(cam_graph, gen_model, timestamps, route_length, d_refine=sample_d_refine,
-                                     num_inter_cut_off=num_inter_cut_off, num_pos_route_cap=100)
+                                     num_inter_cut_off=num_inter_cut_off, num_pos_route_cap=10)
     true_polylines = [bmm.observation_time_rows(rou)[:, 5:7] for rou in routes]
     routes_obs_rows = [bmm.observation_time_rows(rou) for rou in routes]
     len_routes = [len(rou) for rou in routes]
@@ -68,10 +68,9 @@ n_iter = 100
 tune_model = bmm.GammaMapMatchingModel()
 tune_model.distance_params['a_speed'] = 1.
 tune_model.distance_params_bounds['a_speed'] = (1., 1.)
-# tune_model.distance_params['b_speed'] = 0.19
 tune_model.distance_params['b_speed'] = 0.05
-tune_model.zero_dist_prob_neg_exponent = -np.log(0.05)/timestamps
-# tune_model.deviation_beta = 1/12
+tune_model.zero_dist_prob_neg_exponent = -np.log(0.08)/timestamps
+#tune_model.zero_dist_prob_neg_exponent = -np.log(0.05)/timestamps
 tune_model.deviation_beta = 0.05
 tune_model.gps_sd = 7.
 
@@ -87,6 +86,6 @@ tune_model.gps_sd = 7.
 
 params_track = bmm.offline_em(cam_graph, tune_model, timestamps, observations, n_iter=n_iter, max_rejections=0,
                               initial_d_truncate=50, num_inter_cut_off=num_inter_cut_off,
-                              gradient_stepsize_scale=1e-3, gradient_stepsize_neg_exp=0.55)
+                              gradient_stepsize_scale=1e-4, gradient_stepsize_neg_exp=0.75)
 
 
