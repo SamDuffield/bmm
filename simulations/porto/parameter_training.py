2055ecdbd98848ee906eb3bf62554a80ae24f71a
SamDuffield <sddd2@cam.ac.uk>
2020-08-24 07:46:43 +0100

    param params edit

--- simulations/porto/parameter_training.py
+++ simulations/porto/parameter_training.py
@@ -10,7 +10,9 @@ import os
 import json
 import sys
 
-repo_path = os.getcwd()
+sim_dat_path = os.getcwd()
+repo_path = os.path.dirname(os.path.dirname(sim_dat_path))
+sys.path.append(sim_dat_path)
 sys.path.append(repo_path)
 
 import numpy as np
@@ -22,15 +24,15 @@ import bmm
 np.random.seed(0)
 
 timestamps = 15
-n_iter = 50
+n_iter = 200
 n_particles = 100
 
 graph_path = os.getcwd() + '/portotaxi_graph_portugal-140101.osm._simple.graphml'
-graph_path = repo_path + '/simulations/porto-real/portotaxi_graph_portugal-140101.osm._simple.graphml'
+#graph_path = repo_path + '/simulations/porto-real/portotaxi_graph_portugal-140101.osm._simple.graphml'
 graph = ox.load_graphml(graph_path)
 
 train_data_path = os.getcwd() + '/training_data.csv'
-train_data_path = repo_path + '/simulations/porto-real//training_data.csv'
+#train_data_path = repo_path + '/simulations/porto-real//training_data.csv'
 # Load long-lat polylines
 polylines_ll = [np.array(json.loads(poly)) for poly in pd.read_csv(train_data_path)['POLYLINE']]
 # Convert to utm
@@ -39,9 +41,9 @@ polylines = [bmm.long_lat_to_utm(poly, graph) for poly in polylines_ll]
 # Initiate model
 mm_model = bmm.ExponentialMapMatchingModel()
 # mm_model.zero_dist_prob_neg_exponent = -np.log(0.2) / timestamps
-mm_model.distance_params['zero_dist_prob_neg_exponent'] = -np.log(0.2) / timestamps
+mm_model.distance_params['zero_dist_prob_neg_exponent'] = -np.log(0.15) / timestamps
 mm_model.distance_params['lambda_speed'] = 1 / 10
-mm_model.deviation_beta = 0.01
+mm_model.deviation_beta = 0.1
 mm_model.gps_sd = 7.
 
 params_track = bmm.offline_em(graph, mm_model, timestamps, polylines,
